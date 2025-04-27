60f63934757d9f4551c98a85606d6159e760943d
twothirdshuman <axelwas07@gmail.com>
2025-04-27 22:39:18 +0200

    aaa

--- atm.lua
+++ atm.lua
@@ -4,11 +4,11 @@ monitor.setTextScale(0.5)
 local width, height = monitor.getSize()
 
 monitor.setCursorPos(1, 1)
-monitor.write("check balance")
-monitor.setCursorPos(1, 2)
-monitor.write("do transaction")
+monitor.blit("Check balance", colors.green)
+monitor.setCursorPos(1, 3)
+monitor.write("Do transaction", colors.blue)
 
 while true do
     local _, _, x, y = os.pullEvent("monitor_touch")
-    print(x, y)
+    
 end
